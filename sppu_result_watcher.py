7966e03df415117d355c1fe15a59f9b412a7e579
Aniruddha-Sonawane <aniruddhasonawane2@gmail.com>
2026-02-08 18:07:16 +0530

    Testing

--- sppu_result_watcher.py
+++ sppu_result_watcher.py
@@ -6,7 +6,7 @@ import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-URL = "https://onlineresults.unipune.ac.in/SPPU"
+URL = "https://aniruddha-sonawane.github.io/SPPU-Result-Clone/"
 DATA_FILE = "known_results.json"
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
